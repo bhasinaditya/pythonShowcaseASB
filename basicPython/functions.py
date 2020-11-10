ce52c05bfcdde74b99e24c9dbc5edf0c5aa1e0b9
adityabhasin <adityabhasin8@gmail.com>
2020-11-09 22:05:04 -0500

    new stuff

---
+++ basicPython/functions.py
@@ -0,0 +1,24 @@
+##############################################################
+# Function practice
+
+def hello():
+    print('Hello!')
+    print('I like Chipotle')
+    print('Let me get carne asada')
+
+
+hello()
+hello()
+hello()
+
+
+# Parameterized functions
+
+def paramFunc(name):
+    print('Hello, ' + name + '!')
+
+
+# passing in a value to the function
+paramFunc('Melissa')
+
+##############################################################
---
+++ smallPrograms/magic8ball.py
@@ -0,0 +1,32 @@
+##############################################################
+# Magic 8 ball
+
+import random
+
+
+def getAnswer(answerNumber):
+    if answerNumber == 1:
+        return 'It is certain'
+    elif answerNumber == 2:
+        return 'It is decidedly so'
+    elif answerNumber == 3:
+        return 'Yes'
+    elif answerNumber == 4:
+        return 'Reply hazy, try again'
+    elif answerNumber == 5:
+        return 'Ask again later'
+    elif answerNumber == 6:
+        return 'Concentrate and ask again'
+    elif answerNumber == 7:
+        return 'My reply is no'
+    elif answerNumber == 8:
+        return 'Outlook is unclear'
+    elif answerNumber == 9:
+        return 'Very Doubtful'
+
+
+r = random.randint(1, 9)
+fortune = getAnswer(r)
+print(fortune)
+
+##############################################################
