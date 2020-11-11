bf379b70e854cbe9ab3cc69d3e1aa7bfdcb30380
adityabhasin <adityabhasin8@gmail.com>
2020-11-10 21:36:20 -0500

    localGlobalNames

---
+++ basicPython/localGlobalSameName.py
@@ -0,0 +1,20 @@
+######################################################
+# program to show global/local naming
+
+def spam():
+    eggs = 'spam local'
+    print (eggs)    # prints spam local
+
+
+def bacon():
+    eggs = 'bacon local'
+    print(eggs)    # prints bacon local
+    spam()
+    print(eggs)    # prints bacon local
+
+
+eggs = 'global'
+bacon()
+print(eggs)
+
+######################################################
