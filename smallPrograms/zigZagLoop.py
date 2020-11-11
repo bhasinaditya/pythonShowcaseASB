543d2999e96e0e189123abf2c69730116f7cce5b
adityabhasin <adityabhasin8@gmail.com>
2020-11-10 22:21:41 -0500

    ZigZag

---
+++ smallPrograms/zigZagLoop.py
@@ -0,0 +1,32 @@
+######################################################
+# Program that prints * in a zigzag format
+
+import time, sys
+
+indent = 0  # how many spaces to indent
+indentIncreasing = True  # Whether the indentation is increasing or not
+
+try:
+    while True:  # The main program loop
+        print(' ' * indent, end='')
+        print('*******')
+        time.sleep(0.08)  # pause for 1/10 second interval
+
+        if indentIncreasing:
+            # Increase the number of spaces
+            indent = indent + 1
+            if indent == 20:
+                # change direction
+                indentIncreasing = False
+        else:
+            # decrease the number of spaces
+            indent = indent - 1
+            if indent == 0:
+                # change direction
+                indentIncreasing = True
+
+except KeyboardInterrupt:
+    print('Program Stopped...')
+    sys.exit()
+
+######################################################
